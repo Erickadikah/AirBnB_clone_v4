b991f0c7ae16e37a0b42708e82fd23317c1911b9
erickadikah <erickadikah2030@gmail.com>
2023-01-30 09:23:37 +0300

    task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -33,43 +33,59 @@ $(function () {
     }
   });
 
+  $('button').on('click', function (e) {
+    const placeSection = $('section.places');
+    const amenityIds = amenities.map(function (d) {
+      return d.id.replace(':', '');
+    });
+    placeSection.empty();
+    placesSearch({ amenities: amenityIds });
+  });
+
+  placesSearch();
+});
+
+const placesSearch = function (query) {
+  query = query || {};
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    data: JSON.stringify({}),
+    data: JSON.stringify(query),
     dataType: 'json',
     success: function (data) {
       const placeSection = $('section.places');
 
       for (const place of data) {
         const article = `
-        <article>
-          <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">$${place.price_by_night}</div>
-          </div>
-          <div class="information">
-            <div class="max_guest">
-            ${place.max_guest} Guest${place.max_guest > 1 ? 's' : ''}
-            </div>
-            <div class="number_rooms">
-            ${place.number_rooms} Bedroom${place.number_rooms > 1 ? 's' : ''}
-            </div>
-            <div class="number_bathrooms">
-            ${place.number_bathrooms} Bathroom${
+              <article>
+                <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">$${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">
+                  ${place.max_guest} Guest${place.max_guest > 1 ? 's' : ''}
+                  </div>
+                  <div class="number_rooms">
+                  ${place.number_rooms} Bedroom${
+          place.number_rooms > 1 ? 's' : ''
+        }
+                  </div>
+                  <div class="number_bathrooms">
+                  ${place.number_bathrooms} Bathroom${
           place.number_bathrooms > 1 ? 's' : ''
         }
-            </div>
-          </div>
-          <div class="description">
-            ${place.description}
-          </div>
-      </article>
-      `;
+                  </div>
+                </div>
+                <div class="description">
+                  ${place.description}
+                </div>
+            </article>
+            `;
         placeSection.append(article);
       }
     }
   });
-});
+};
 
