8f4655d0799854dcc3a18f2380ffe850882e0551
kwabenasapong <kwabenasapong@gmail.com>
2023-01-30 15:03:16 +0000

    2023-01-30 15:03:15 - 100-hbnb.sql 3-index.html README.md 8-index.html 102-filte

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,6 +29,7 @@ def not_found(error):
     """
     return make_response(jsonify({'error': "Not found"}), 404)
 
+
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
     'uiversion': 3
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,12 @@
-$('document').ready(function() {
-    let checked = {};
-    $('input[type="checkbox"]').change(function() {
-        if (this.checked == true ) {
-            checked[$(this).attr('data-id')] = $(this).attr('data-name');
-        } else {
-            delete checked[$(this).attr('data-id')];
-        }
-        $('.amenities h4').text(Object.values(checked).join(', '));
-    });
+$('document').ready(function () {
+  const check = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked == true) {
+      check[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete check[$(this).attr('data-id')];
+    }
+    console.log(Object.values(check));
+    $('.amenities h4').text(Object.values(check).join(', '));
+  });
 });
